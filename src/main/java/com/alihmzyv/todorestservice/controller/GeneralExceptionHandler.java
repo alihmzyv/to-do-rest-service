273dfb61515a0ba9bfd0db320152b3eff77fa046
alihmzyv <alihmzyv@gmail.com>
2023-03-17 17:20:27 +0400

    handle access denied exception

--- src/main/java/com/alihmzyv/todorestservice/controller/GeneralExceptionHandler.java
+++ src/main/java/com/alihmzyv/todorestservice/controller/GeneralExceptionHandler.java
@@ -5,12 +5,16 @@ import com.alihmzyv.todorestservice.exception.security.CustomAuthenticationExcep
 import com.alihmzyv.todorestservice.exception.security.CustomAuthorizationException;
 import com.alihmzyv.todorestservice.model.dto.base.BaseResponse;
 import com.alihmzyv.todorestservice.model.dto.error.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -72,5 +76,12 @@ public class GeneralExceptionHandler {
                 .build();
         return ResponseEntity.badRequest()
                 .body(resp);
+    }
+
+    @ExceptionHandler (value = {AccessDeniedException.class})
+    public void handleAccessDeniedException(HttpServletRequest request, HttpServletResponse response,
+                                            AccessDeniedException accessDeniedException) throws IOException {
+        // 403
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Authorization Failed : " + accessDeniedException.getMessage());
     }
 }
