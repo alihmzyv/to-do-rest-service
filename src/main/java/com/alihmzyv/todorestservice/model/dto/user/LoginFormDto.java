ba490d554e54bec1d84d74124a6c6304d05da80e
alihmzyv <alihmzyv@gmail.com>
2023-03-21 12:21:55 +0400

    fix security path match bug.

--- src/main/java/com/alihmzyv/todorestservice/model/dto/user/LoginFormDto.java
+++ src/main/java/com/alihmzyv/todorestservice/model/dto/user/LoginFormDto.java
@@ -5,9 +5,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 @Data
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class LoginFormDto {
     @Schema(description = "{email.valid}", example = "alihmzyv@gmail.com")
